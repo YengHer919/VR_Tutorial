29b266051e35268723985248633e57b9ca0b2a6c
Yeng Her <119150603+YengHer919@users.noreply.github.com>
2026-09-01 19:17:24 -0700

    Tut 6
    Added ray and poke interactables with UI

--- Assets/TeleportationActivator.cs
+++ Assets/TeleportationActivator.cs
@@ -4,6 +4,7 @@ using UnityEngine.XR.Interaction.Toolkit.Interactors;
 public class TeleportationActivator : MonoBehaviour
 {
     public XRRayInteractor teleportInteractor;
+    public XRRayInteractor rayInteractor;
     public InputActionProperty teleportActivator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -11,11 +12,21 @@ public class TeleportationActivator : MonoBehaviour
     {
         teleportInteractor.gameObject.SetActive(false);
         teleportActivator.action.performed += Action_performed;
+        rayInteractor.uiHoverEntered.AddListener(x => disableTeleport());
     }
 
     private void Action_performed(InputAction.CallbackContext obj)
     {
+        if (rayInteractor && rayInteractor.IsOverUIGameObject())
+        {
+            return;
+        }
         teleportInteractor.gameObject.SetActive(true);
+    }
+
+    public void disableTeleport()
+    {
+        teleportInteractor.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
